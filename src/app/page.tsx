bc6f7e1796a60b2099cf1632a5c8500a0f4968b7
Ingrid Bjerre <165039166+ingrididk@users.noreply.github.com>
2026-02-05 17:57:49 +0000

    delar av place order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,64 @@
 "use client";
-import {getMenuOptions} from "src/api/@tanstack/react-query.gen.ts";
-import {useQuery} from "@tanstack/react-query"
+import { getMenuOptions } from "src/api/@tanstack/react-query.gen.ts";
+import { postOrdersMutation } from "src/api/@tanstack/react-query.gen.ts";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Page() {
-	function onButtonClick() {
-		alert("<3");
-	}
-
 	const { data } = useQuery(getMenuOptions());
-	console.log(data)
 
+	//för att lägga beställningar:
+	const adressInput: HTMLInputElement = document.getElementById(
+		"adress",
+	) as HTMLInputElement;
+	let adress: string;
+
+	const noteInput: HTMLINputElement = document.getElementById(
+		"note",
+	) as HTMLInputElement;
+	let note: string;
+
+	function onButtonClick() {
+		adress = adressInput.value;
+		note = noteInput.value;
+	}
 
 	return (
 		<div className="space-y-4">
+			<ul className="list-disc">
+				{data?.map((x) => (
+					<li key={x.id}>
+						{" "}
+						<b>{x.name}</b> | {x.price} kr | Antal:
+						<select id={String(x.id)}>
+							<option value="0">0</option>
+							<option value="1">1</option>
+							<option value="2">2</option>
+							<option value="3">3</option>
+						</select>
+						<ol> {x.description} </ol>{" "}
+					</li>
+				))}
+			</ul>
+
+			<p>
+				Adress: <input type="text" id="adress" /> <br />
+				Kommentar: <input type="text" id="note" />
+			</p>
+
+			{/* <form onSubmit={(e) => {   
+  		e.preventDefault(); 
+ 			adress = e.target.value;
+			}}>
+			<label for="adress" >Adress:</label> <input type="text" id="adress" name="adress"/>
+		</form> */}
+
 			<button
+				type="button"
 				className="bg-amber-400 shadow-sm text-lg font-medium hover:bg-amber-500 active:bg-amber-600 border-amber-500 border-2 px-4 py-2 text-white rounded-md"
 				onClick={onButtonClick}
 			>
-				En fin knapp
+				Beställ
 			</button>
-
-			<p>
-				<a
-					href="https://enclose.horse/"
-					target="_blank" // open in a new browser tab
-					className="underline text-blue-500 hover:text-blue-600"
-					rel="noreferrer"
-				>
-					En mystisk länk
-				</a>
-			</p>
-
-			<ul className="list-disc">
-				{data?.map( x => (<li key={x.id}> <b>{x.name}</b> | {x.price} kr <ol> {x.description} </ol> </li>))}
-			</ul>
-
-			
-
 		</div>
 	);
 }
