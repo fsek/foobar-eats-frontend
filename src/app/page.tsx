3937d5186a78c6d1a7e7825c0bf458999eb0f880
Ingrid Bjerre <165039166+ingrididk@users.noreply.github.com>
2026-02-19 17:50:02 +0000

    allt klart?!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 "use client";
-import { getMenuOptions } from "src/api/@tanstack/react-query.gen.ts";
+import {
+	getMenuOptions,
+	getOrdersByIdOptions,
+} from "src/api/@tanstack/react-query.gen.ts";
 import { postOrdersMutation } from "src/api/@tanstack/react-query.gen.ts";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 import { z } from "zod";
 import { useState } from "react";
@@ -10,14 +13,58 @@ import MenuItemButton from "./MenuItemButton";
 import OrderButton from "./OrderButton";
 import InputLine from "./InputLine";
 import { IterationCcw } from "lucide-react";
+import type { Order } from "@/api";
 
 export default function Page() {
 	const { data } = useQuery(getMenuOptions());
 
 	const [order, setOrder] = useState<number[]>([]);
-
 	const [adress, setAdress] = useState<string>("");
 	const [note, setNote] = useState<string>("");
+
+	const [orderResponse, setOrderResponse] = useState<Order | undefined>();
+
+	const [requestedOrderId, setRequestedOrderId] = useState<number>(-1);
+
+	const { data: data2, error } = useQuery({
+		enabled: requestedOrderId !== -1,
+		...getOrdersByIdOptions({ path: { id: requestedOrderId ?? -1 } }),
+	});
+	console.log(data2);
+
+	const postOrder = useMutation({
+		...postOrdersMutation(),
+		onSuccess: (variables) => {
+			setOrderResponse(variables);
+		},
+		onError: () => {
+			alert("Orderplacering misslyckades");
+		},
+	});
+
+	function onOrderClick() {
+		postOrder.mutate({
+			body: {
+				items: order,
+				address: adress,
+				note: note,
+			},
+		});
+		setOrder([]);
+		setAdress("");
+		setNote("");
+		alert(
+			`Tack för din beställning. Total: ${orderResponse?.total}. Order-id: ${orderResponse?.id}`,
+		);
+	}
+
+	function seeOrderClick() {
+		if (data2) {
+			alert(`Orderstatus: ${data2.status}`);
+		} else {
+			alert("Ingen sådan order.");
+		}
+	}
 
 	return (
 		<div className="space-y-4">
@@ -36,7 +83,22 @@ export default function Page() {
 			<InputLine addText={(x) => setAdress(x)} text="Note" />
 			<br />
 
-			{/* <OrderButton itemsOrdered={order} adress={adress} note={note} placeOrder={()=> postOrdersMutation({order, adress, note} )}/> */}
+			<button
+				type="button"
+				onClick={onOrderClick}
+				className="bg-amber-400 shadow-sm text-lg font-medium hover:bg-amber-500 active:bg-amber-600 border-amber-500 border-2 px-4 py-2 text-white rounded-md"
+			>
+				Beställ
+			</button>
+
+			<InputLine addText={(x) => setRequestedOrderId(x)} text="Order id" />
+			<button
+				type="button"
+				onClick={seeOrderClick}
+				className="bg-amber-400 shadow-sm text-lg font-medium hover:bg-amber-500 active:bg-amber-600 border-amber-500 border-2 px-4 py-2 text-white rounded-md"
+			>
+				Se orderstatus
+			</button>
 		</div>
 	);
 }
